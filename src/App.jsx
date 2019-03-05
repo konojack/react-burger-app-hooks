9fc2094cd9476c29d32553c89d24d29e2215dbf3
Dawid Konojacki <dawid.konojacki@sb-betting.com>
2019-03-05 16:59:49 +0100

    Adding main layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Layout from 'components/Layout/Layout'
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Layout>
+          <h2>Test</h2>
+        </Layout>
       </div>
     );
   }
