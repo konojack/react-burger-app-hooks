630eb361b11c007dfec0f6acc79c2d76c45361e4
konojack <konojack@gmail.com>
2019-11-16 18:58:56 +0100

    Adding actions to Auth component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
 import classes from './Auth.module.scss';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+
+import * as actions from '../../store/actions/';
 
 export class Auth extends Component {
     state = {
@@ -75,6 +78,11 @@ export class Auth extends Component {
         this.setState({controls: updatedControls});
     }
 
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value)
+    }
+
 
     render() {
         const formElementsArray = [];
@@ -99,7 +107,7 @@ export class Auth extends Component {
         ))
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
                 </form>
@@ -108,4 +116,10 @@ export class Auth extends Component {
     }
 }
 
-export default Auth
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password)) 
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Auth);
