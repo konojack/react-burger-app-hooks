ab4b1b416c05fb6b55ca8177c0f5fc7c36abcf42
konojack <konojack@gmail.com>
2019-11-24 17:11:13 +0100

    Updates

---
+++ precache-manifest.ce107101638317f640d47900436008af.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "586593ad5a423db623fb",
+    "url": "/react-burger-app/static/css/4.5ebdcb48.chunk.css"
+  },
+  {
+    "revision": "1256af1b43159f2b3929",
+    "url": "/react-burger-app/static/css/main.2edbf74b.chunk.css"
+  },
+  {
+    "revision": "9f7912c951705eff1f34",
+    "url": "/react-burger-app/static/js/runtime~main.9f7912c9.js"
+  },
+  {
+    "revision": "1e930494d4efae7b7c32",
+    "url": "/react-burger-app/static/js/2.1e930494.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react-burger-app/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "bd32282d67ac2d25a1d6",
+    "url": "/react-burger-app/static/js/3.bd32282d.chunk.js"
+  },
+  {
+    "revision": "1256af1b43159f2b3929",
+    "url": "/react-burger-app/static/js/main.1256af1b.chunk.js"
+  },
+  {
+    "revision": "586593ad5a423db623fb",
+    "url": "/react-burger-app/static/js/4.586593ad.chunk.js"
+  },
+  {
+    "revision": "7c121439c2fab7d006e8",
+    "url": "/react-burger-app/static/js/5.7c121439.chunk.js"
+  },
+  {
+    "revision": "7c121439c2fab7d006e8",
+    "url": "/react-burger-app/static/css/5.bc9df90e.chunk.css"
+  },
+  {
+    "revision": "bd32282d67ac2d25a1d6",
+    "url": "/react-burger-app/static/css/3.6022030f.chunk.css"
+  },
+  {
+    "revision": "c669dbff9d55e128aad9e5f893a0f56c",
+    "url": "/react-burger-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/burger-app/precache-manifest.225ccd94f2334b96a48d845baeffcbda.js"
+  "/react-burger-app/precache-manifest.ce107101638317f640d47900436008af.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/burger-app/index.html", {
+workbox.routing.registerNavigationRoute("/react-burger-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
