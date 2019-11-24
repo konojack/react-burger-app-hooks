024fea8429847a789cbd9647f622c2ad485642d5
konojack <konojack@gmail.com>
2019-11-24 17:08:30 +0100

    Updates

---
+++ precache-manifest.225ccd94f2334b96a48d845baeffcbda.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "586593ad5a423db623fb",
+    "url": "/burger-app/static/css/4.5ebdcb48.chunk.css"
+  },
+  {
+    "revision": "ac1724905809930bca8f",
+    "url": "/burger-app/static/css/main.2edbf74b.chunk.css"
+  },
+  {
+    "revision": "18dc89f5c942c7171f49",
+    "url": "/burger-app/static/js/runtime~main.18dc89f5.js"
+  },
+  {
+    "revision": "1e930494d4efae7b7c32",
+    "url": "/burger-app/static/js/2.1e930494.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-app/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "bd32282d67ac2d25a1d6",
+    "url": "/burger-app/static/js/3.bd32282d.chunk.js"
+  },
+  {
+    "revision": "ac1724905809930bca8f",
+    "url": "/burger-app/static/js/main.ac172490.chunk.js"
+  },
+  {
+    "revision": "586593ad5a423db623fb",
+    "url": "/burger-app/static/js/4.586593ad.chunk.js"
+  },
+  {
+    "revision": "7c121439c2fab7d006e8",
+    "url": "/burger-app/static/js/5.7c121439.chunk.js"
+  },
+  {
+    "revision": "7c121439c2fab7d006e8",
+    "url": "/burger-app/static/css/5.bc9df90e.chunk.css"
+  },
+  {
+    "revision": "bd32282d67ac2d25a1d6",
+    "url": "/burger-app/static/css/3.6022030f.chunk.css"
+  },
+  {
+    "revision": "220d0a98e0ad4898c3051263cd612efe",
+    "url": "/burger-app/index.html"
+  }
+];
